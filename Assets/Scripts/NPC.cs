e7b724920027de11947b57b0c4b2a8721d6f3aa1
COBORAX <Cobo@Mikkels-Mac-Pro.local>
2015-10-11 20:28:07 +0200

    Basic Controls
    I added basic controls to the game

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPC : MonoBehaviour {
+
+	public GameObject talk;
+	public GameObject dialog;
+	
+
+	void OnTriggerEnter2D (Collider2D other) {
+		talk.SetActive (true);
+	}
+
+	void OnTriggerStay2D (Collider2D other) {
+		if (Input.GetKeyDown (KeyCode.E)) {
+			dialog.SetActive (true);
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D other) {
+		talk.SetActive (false);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 10f;
+	public Animator anim;
+
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Movement input
+		float hMove = Input.GetAxisRaw ("Horizontal");
+		float vMove = Input.GetAxisRaw ("Vertical");
+
+		//Check if the player moves
+		if (hMove == 1) {
+			transform.Translate (Vector2.right * speed * Time.deltaTime);
+		}
+		if (hMove == -1) {
+			transform.Translate (Vector2.left * speed * Time.deltaTime);
+		}
+
+		if (vMove == 1) {
+			transform.Translate (Vector2.up * speed * Time.deltaTime);
+		}
+		if (vMove == -1) {
+			transform.Translate (Vector2.down * speed * Time.deltaTime);
+		}
+
+		//Check if player is attacking
+		if (Input.GetButton ("Fire1")) {
+			anim.SetBool ("Attack", true);
+		} else {
+			anim.SetBool ("Attack", false);
+		}
+
+		//Check if player is jumping
+		if (Input.GetButtonDown ("Jump")) {
+			anim.SetBool ("Jumping", true);
+		} else {
+			anim.SetBool("Jumping", false);
+		}
+
+	}
+}
